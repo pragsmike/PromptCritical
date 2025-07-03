42538477f4973f11900a623a9d861cdfa8a87a60
Mike Gallaher <152364+pragsmike@users.noreply.github.com>
2025-07-03 00:24:21 -0400

    Moar pop.

--- components/pop/test/pcrit/pop/core_test.clj
+++ components/pop/test/pcrit/pop/core_test.clj
@@ -1,5 +1,13 @@
 (ns pcrit.pop.core-test
-  (:require [clojure.test :refer (deftest is)]))
+  (:require [clojure.test :refer (deftest is use-fixtures)]
+            [clojure.java.io :as io]
+            [pcrit.pop.core :as pop]
+            [prcit.pop.temp-dir :refer [with-temp-dir *tmp-dir*]]))
+
+(use-fixtures :each with-temp-dir)
+
 
 (deftest test-bootstrap
-  )
+  (pop/bootstrap *tmp-dir* "hi")
+  (let [pf (io/file *tmp-dir* "P1.prompt")]
+    (is (.exists pf))))
